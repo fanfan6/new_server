c9400e90602deb312104a8de71577d1b31d4292b
fanfan <fanfan12@aliyun.com>
2018-02-27 16:01:26 +0800

    c1

--- admin/settings.py
+++ admin/settings.py
@@ -90,7 +90,7 @@ DATABASES = {
         'NAME': 'online',
         'USER': 'root',
         'PASSWORD': 'mysql',
-        'HOST': '127.0.0.3',
+        'HOST': '127.0.0.1',
         'PORT': '3306',
     }
 }
