5342ee6e13e1b502b1d6bfaf7ff785890fde1586
fanfan <fanfan12@aliyun.com>
2018-02-27 15:55:17 +0800

    check

--- admin/settings.py
+++ admin/settings.py
@@ -90,7 +90,7 @@ DATABASES = {
         'NAME': 'online',
         'USER': 'root',
         'PASSWORD': 'mysql',
-        'HOST': '127.0.0.1',
+        'HOST': '127.0.0.2',
         'PORT': '3306',
     }
 }
